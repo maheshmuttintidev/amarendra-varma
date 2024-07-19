d024df4ddf4f1727a69b96fc4eaf47d78b0c9f5a
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-07-19 19:28:26 +0530

    happy birthdat amarendra

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,28 @@
-import './globals.css'
+import "./globals.css";
+import styles from "./page.module.css";
+import { Comic_Neue } from "@next/font/google";
+
+const comic = Comic_Neue({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "700"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={comic.className}>
       <head />
-      <body>{children}</body>
+      <body>
+        <video className={styles.videoBackground} autoPlay loop muted>
+          <source src={"/bg-video.mp4"} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,101 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import dynamic from "next/dynamic";
 import styles from "./page.module.css";
-import birthDayPic from "../../public/group_persons_with_hat.jpg";
+import "./globals.css";
+const Confetti = dynamic(() => import("react-confetti"), { ssr: false });
+import IronMan from "../../public/iron_man.png";
+import CaptainAmericaShield from "../../public/captain_america_shield.png";
 
-const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
 
 export default function Home() {
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    const birthDate = new Date("1997-07-19");
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const nextBirthdayDate = new Date(
+      currentYear,
+      birthDate.getMonth(),
+      birthDate.getDate()
+    );
+
+    if (today > nextBirthdayDate) {
+      nextBirthdayDate.setFullYear(currentYear + 1);
+    }
+
+    setAge(currentYear - birthDate.getFullYear());
+  }, []);
+
+  const audioRefs = useRef([]);
+  const audioSources = ["/audio_1.mp3"];
+
+  useEffect(() => {
+    const playNextAudio = (index: any) => {
+      if (index >= audioSources.length) {
+        index = 0;
+      }
+      const audio = new Audio(audioSources[index]);
+      // @ts-ignore
+      audioRefs.current[index] = audio;
+      audio.play();
+      audio.addEventListener("ended", () => playNextAudio(index + 1));
+    };
+
+    const init = async () => {
+      await navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(() => {
+          playNextAudio(0);
+        })
+        .catch(() => {
+          console.log("audio permission denied");
+        });
+    };
+    init();
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Ganga Babu Rachamanti&nbsp;
-          <code className={styles.code}>PUBG Gamer and Family Man</code>
-        </p>
+    <div className={styles.container}>
+      <Image
+        src={IronMan}
+        alt="iron man"
+        className="object-contain h-40 pt-5"
+      />
+      <h1 className="text-3xl font-bold py-3">
+        Wish You Happy Birthday Amarendra Varma!
+      </h1>
+      <div className={styles.threedlook}>
+        <section className="px-3 text-white py-5 rounded-md flex flex-row mx-auto bg-slate-300 flex-wrap gap-7 bg-[url('/avengers_bg_1.jpg')] bg-no-repeat bg-cover justify-center items-center shadow-xl">
+          <div>
+            <h2 className="text-xl font-bold bg-black px-3 py-2 rounded-md">
+              July 19, 1997
+            </h2>
+            <div className="relative">
+              <div className="object-contain w-screen md:w-[300px] mt-4 rounded-full p-3 h-60 inline-flex gap-10 justify-between items-center border-4 border-yellow-400">
+                <h1 className="text-3xl md:text-4xl text-white font-bold">
+                  Amarendra Varma
+                </h1>
+                <Image
+                  src={CaptainAmericaShield}
+                  alt="iron man"
+                  className="object-contain"
+                />
+              </div>
+              <p className="text-3xl absolute bottom-[-10px] shadow-black shadow-lg left-[50%] translate-x-[-50%] text-yellow-600 font-bold bg-yellow-100 rounded-full h-12 w-12 flex justify-center items-center border border-2 border-yellow-500">
+                {age}
+              </p>
+            </div>
+          </div>
+          <div className="relative">
+            <div className={styles.firecrackers}>🎉🎇✨</div>
+            <div className={styles.giftBox}>🎁</div>
+          </div>
+        </section>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/gangaBabu.jpg"
-          alt="Ganga babu profile pic"
-          width={200}
-          height={200}
-          priority
-        />
-        {/* <div className={styles.thirteen}>
-          <Image src="/gangaBabu.jpg" alt="13" width={40} height={31} priority />
-        </div> */}
-      </div>
-
-      <div className={styles.fontMono}>
-        <p className={styles.center}>
-          8th Feb 2023 Came - Wish You a Happy Birth Day. May God bless you and
-          protect you with Abundant Love and Grace.
-        </p>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src={birthDayPic}
-          alt="Birth Day Friends Logo"
-          width={300}
-          priority
-        />
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/birthday_friends_with_decorations.avif"
-          alt="Birth Day Friends Logo"
-          width={300}
-          height={150}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Photos <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Find My Uploaded Pictures.</p>
-        </a>
-
-        <a
-          href="/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Videos <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore Videos Uploaded.</p>
-        </a>
-
-        <a
-          href="/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Contact <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>To Connect with me.</p>
-        </a>
-      </div>
-    </main>
+      <Confetti />
+    </div>
   );
 }
